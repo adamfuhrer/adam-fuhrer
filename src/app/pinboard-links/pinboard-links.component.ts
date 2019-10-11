6675d81bdfd7a66514356dfa2ec49d52a10f1359
adamfuhrer <adam.fuhrer@caseware.com>
2019-10-11 14:51:50 -0400

    add pinboard links component

---
+++ src/app/pinboard-links/pinboard-links.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-pinboard-links',
+    templateUrl: './pinboard-links.component.html',
+    styleUrls: ['./pinboard-links.component.scss']
+})
+export class PinboardLinksComponent implements OnInit {
+    ngOnInit() {
+    }
+}
