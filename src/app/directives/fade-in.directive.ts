00fa696b8d55d1238e194f684dee87101c39b946
adamfuhrer <adam.fuhrer@caseware.com>
2019-12-13 01:21:59 -0500

    fix layout issues in Safari

--- src/app/directives/fade-in.directive.ts
+++ src/app/directives/fade-in.directive.ts
@@ -7,22 +7,22 @@ import {animate, AnimationBuilder, style} from '@angular/animations';
 })
 export class FadeInDirective  implements OnChanges {
     @Input('fadeIn') fadeIn: boolean;
-    @Input() fadeDuration = '600ms';
+    @Input() fadeDuration = '700ms';
     fadeAnimation: any;
     player: any;
 
     constructor(private builder: AnimationBuilder,
                 private el: ElementRef) {
-        this.fadeAnimation = this.builder.build([
-            style({opacity: 0}),
-            animate(this.fadeDuration + ' cubic-bezier(0.215, 0.61, 0.355, 1)', style({opacity: 1}))
-        ]);
-
-        this.player = this.fadeAnimation.create(this.el.nativeElement);
     }
 
     ngOnChanges(changes: SimpleChanges) {
         if (changes['fadeIn']) {
+            this.fadeAnimation = this.builder.build([
+                style({opacity: 0}),
+                animate(this.fadeDuration + ' cubic-bezier(0.215, 0.61, 0.355, 1)', style({opacity: 1}))
+            ]);
+
+            this.player = this.fadeAnimation.create(this.el.nativeElement);
             this.player.play();
         }
     }
