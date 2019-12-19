018f286d4618f5df3c0662f7aa106aa5855eed72
adamfuhrer <adam.fuhrer@caseware.com>
2019-12-18 21:52:36 -0500

    update portraits, update links to "_blank", and other style improvements

--- src/app/components/projects/portraits/portraits.component.ts
+++ src/app/components/projects/portraits/portraits.component.ts
@@ -14,6 +14,18 @@ export class PortraitsComponent implements OnInit {
     constructor(private navigationService: NavigationService,
                 private titleService: Title,
                 private metaService: Meta) {
+        this.metaService.addTags([
+            { property: 'og:title', content: '28 Portraits by 28 Photographers'},
+            { property: 'og:description', content: 'A selection of phenomenal portrait photography.'},
+            { property: 'og:url', content: 'https://adamfuhrer.com/28-portraits-by-28-photographers'},
+            { name: 'twitter:title', content: '28 Portraits by 28 Photographers' },
+            { name: 'twitter:description', content: 'A selection of phenomenal portrait photography.' },
+            { name: 'twitter:url', content: 'https://adamfuhrer.com/28-portraits-by-28-photographers'},
+            { name: 'twitter:card', content: 'summary'},
+            { name: 'facebook:title', content: '28 Portraits by 28 Photographers' },
+            { name: 'facebook:description', content: 'A selection of phenomenal portrait photography.' },
+            { name: 'facebook:url', content: 'https://adamfuhrer.com/28-portraits-by-28-photographers'}
+        ]);
     }
 
     ngOnInit() {
