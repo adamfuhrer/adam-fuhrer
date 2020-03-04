831d455a1149a7870ca5d5c71c6820e857bdd2af
adamfuhrer <adam.fuhrer@caseware.com>
2020-03-03 20:18:51 -0500

    add Google Analytics page tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {NavigationEnd, Router} from '@angular/router';
+declare let ga: Function;
 
 @Component({
     selector: 'app-root',
@@ -12,6 +13,8 @@ export class AppComponent {
         router.events.subscribe(nav => {
             if (nav instanceof NavigationEnd) {
                 window.scrollTo(0, 0);
+                ga('set', 'page', nav.urlAfterRedirects);
+                ga('send', 'pageview');
             }
         });
     }
